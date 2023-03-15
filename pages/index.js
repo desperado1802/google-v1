a3cd18bbeab5874ca208eef1c1317350d9494a9d
desperado1802 <mumun_ali@hotmail.com>
2023-03-15 16:51:15 +0200

    created the header section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* Header */}
+      <Header />
+      {/* Body */}
 
-      <h1>Hello Salim</h1>
+      {/* Footer */}
     </>
   );
 }
