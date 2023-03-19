2bee7b29fe0585d1541eb38245b1280b4c737711
desperado1802 <mumun_ali@hotmail.com>
2023-03-19 14:37:09 +0200

    added links to header elements, added functionality to feeling lucky button and added secret

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,15 @@ export default function Home() {
     router.push(`/search?term=${term.trim()}&searchType=`);
   }
 
+  async function randomSearch(event) {
+    event.preventDefault();
+    const randomTerm = await fetch(
+      "https://random-word-api.herokuapp.com/word?number=1"
+    ).then((response) => response.json());
+    if (!randomTerm) return;
+    router.push(`/search?term=${randomTerm}&searchType=`);
+  }
+
   return (
     <>
       <Head>
@@ -53,7 +62,9 @@ export default function Home() {
           <button onClick={search} className="btn">
             Google Search
           </button>
-          <button className="btn">I'm Feeling Lucky</button>
+          <button onClick={randomSearch} className="btn">
+            I&apos;m Feeling Lucky
+          </button>
         </div>
       </form>
       {/* Footer */}
